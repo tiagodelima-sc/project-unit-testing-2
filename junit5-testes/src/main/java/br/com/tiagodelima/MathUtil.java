237256ac92fe173bc8c50bdf1e88c86c849a06c0
Tiago de Lima <tiago.l07@aluno.ifsc.edu.br>
2022-05-15 22:40:03 -0300

    Adicionando novos testes para o MDC

--- junit5-testes/src/main/java/br/com/tiagodelima/MathUtil.java
+++ junit5-testes/src/main/java/br/com/tiagodelima/MathUtil.java
@@ -8,8 +8,30 @@ public class MathUtil {
 
 	public static int mdc(int valorA, int valorB) {
 		
+		valorA = Math.abs(valorA);
+		valorB = Math.abs(valorB);
+		
+		
+		//Propriedade 6
+		//descobrir maior valor
+		final int maiorValor = Math.max(valorA, valorB);
+		
+		valorB = Math.min(valorA, valorB);
+		valorA = maiorValor;
+		
+		//Propriedade 1
 		if(valorB > 0 || valorA == 0) {
 				return valorB;
+		}
+		
+		//Propriedade 3
+		if(valorB == 0) {
+			return Math.abs(valorA);
+		}
+		
+		//Propriedade 5
+		if(valorA % valorB != 0) {
+			return 1;
 		}
 		
 		return -1;
--- junit5-testes/src/test/java/br/com/tiagodelima/MathUtilTest.java
+++ junit5-testes/src/test/java/br/com/tiagodelima/MathUtilTest.java
@@ -2,7 +2,7 @@ package br.com.tiagodelima;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Assertions;
+import static br.com.tiagodelima.MathUtil.mdc;
 import org.junit.jupiter.api.Test;
 
 class MathUtilTest {
@@ -10,14 +10,128 @@ class MathUtilTest {
 	@Test
 	void mdc() {
 		
-		int valorA = 6;
-		int valorB = 3;
-		int resultadoEsperado = 3;
+		final int valorA = 6;
+		final int valorB = 3;
+		final int resultadoEsperado = 3;
 		
 		int resultadoObtido = MathUtil.mdc(valorA, valorB);
 
 		assertEquals(resultadoEsperado, resultadoObtido);
 		
 	}
+	
+
+	@Test
+	void testMdcP2_TestandoNumerosPares() {
+		
+		final int valorA = 16;
+		final int valorB = 8;
+		final int divisor = 4;
+		
+		int resultadoObtido = MathUtil.mdc(valorA, valorB);
+		
+		//verificar se o resto da divisão do resultado obtido é igual a 0
+		assertTrue(resultadoObtido % divisor == 0);
+	}
+	
+	/* Considerando que todo os números são fatores de 0 (pois 0 = 0.a para qualquer a inteiro)
+	 * então mdc(a,0) = |a|;
+	 * */
+	@Test
+	void testMdc_Propriedade3_ValorPositivo(){
+		
+		final int valorA = 15;
+		final int valorB = 0;
+		final int resultadoEsperado = 15;
+		
+		final int resultadoObtido = MathUtil.mdc(valorA, valorB);
+		
+		//verificar se o resultadoObtido é igual resultadoEsperado
+		assertEquals(resultadoEsperado, resultadoObtido);
+		
+	}
+	
+	@Test
+	void testMdc_Propriedade3_ValorNegativo(){
+		
+		final int valorA = -30;
+		final int valorB = 0;
+		final int resultadoEsperado = 30;
+		
+		final int resultadoObtido = MathUtil.mdc(valorA, valorB);
+		
+		//verificar se o resultadoObtido é igual resultadoEsperado
+		assertEquals(resultadoEsperado, resultadoObtido);
+		
+	}
+	
+	/* Se m é um inteiro não negativo 
+	 * então mdc(m.a, m.b) == m.mdc(a,b);
+	 */
+	@Test
+	void testMdc_Propriedade4(){
+		
+		final int valorA = 9;
+		final int valorB = 3;
+		final int m = 2;
+		final int resultadoEsperado = MathUtil.mdc(valorA * valorB, valorB * m);
+		
+		//mdc(a*m, b*m) == m * mdc(a,b)
+		final int resultadoObtido = m * MathUtil.mdc(valorA, valorB);
+		
+		//verificar se o resultadoObtido é igual resultadoEsperado
+		assertEquals(resultadoEsperado, resultadoObtido);
+		
+	}
+	
+	/* Se mdc(a,b) = 1 então
+	 * mdc(a.b,c) = mdc(a,c).mdc(b,c);
+	 */
+	@Test
+	void testMdc_Propriedade5_1(){
+		
+		final int valorA = 7;
+		final int valorB = 3;
+		final int valorC = 3;
+		
+		final int resultadoEsperado = MathUtil.mdc(valorA * valorB, valorC);
+		final int resultadoObtido = MathUtil.mdc(valorB, valorC);
+		
+
+		assertEquals(resultadoEsperado, resultadoObtido);
+		
+	}
+	
+	
+	/* mdc(a,b) = mdc(b,c);
+	 */
+	@Test
+	void testMdc_Propriedade6(){
+		
+		final int valorA = 7;
+		final int valorB = 2;
+		
+		final int resultadoEsperado = MathUtil.mdc(valorA, valorB);
+		final int resultadoObtido = MathUtil.mdc(valorB, valorA);
+		
+		assertEquals(resultadoEsperado, resultadoObtido);
+		
+	}
+	
+	@Test
+	void testMdc_Propriedade7() {
+		final int valorA = -12;
+		final int valorB = -6;
+		
+		final int resultadoEsperado = 6;
+		final int resultadoObtido = MathUtil.mdc(valorA, valorB);
+		
+		assertEquals(resultadoEsperado, resultadoObtido);
+
+
+
+	}
+	
+	
 
 }
